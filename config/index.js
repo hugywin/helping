65175471ebace79d364d3acc05365264ace266a1
huguoyun <huguoyun@yidian-inc.com>
2016-11-08 15:26:23 +0800

    add

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     apply: path.resolve(__dirname, '../dist/apply.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: '',
+    assetsPublicPath: 'http://staticcrowd.iblue.cc/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -19,7 +19,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 80,
     proxyTable: {
       '/api':{
         target:'http://crowd.iblue.cc/api',
